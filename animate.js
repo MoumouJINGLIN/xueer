6eb5b2e409e89c4bf2a4c93ae22661c7394e8398
MoumouJINGLIN <86963838+MoumouJINGLIN@users.noreply.github.com>
2021-09-25 01:09:45 +0800

    Add files via upload

---
+++ animate.js
@@ -0,0 +1,19 @@
+function animate(obj, target, callback) {
+    clearInterval(obj.times)
+    obj.times = setInterval(function() {
+        // 步长值写到定时器的里面
+        // 把我们步长值改为整数 不要出现小数的问题
+        // let step = Math.ceil((target - obj.offsetLeft) / 10)
+        let step = (target - obj.offsetLeft) / 10
+        step = step > 0 ? Math.ceil(step) : Math.floor(step);
+        if (obj.offsetLeft == target) {
+            // 停止动画
+            clearInterval(obj.times)
+        }
+        if (callback) {
+            callback()
+        }
+        // 把每次加1 这个步长值改为一个慢慢变小的值 ，步长公式：（目标值-现在的位置）/10
+        obj.style.left = obj.offsetLeft + step + 'px'
+    }, 15)
+}
